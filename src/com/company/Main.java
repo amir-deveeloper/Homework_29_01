9c659ed1821b67fe0897fbc77df5aa8cf44918a1
amir <khamidullin.amir@gmail.com>
2022-01-18 14:58:19 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,26 @@
 package com.company;
 
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+
+        Integer n = scanner.nextInt();
+
+        int s = 1;
+
+        for (int i = 1; i < n+1; i++){
+
+            s = s*i;
+
+        }
+
+        System.out.println(s);
+
 
     }
 
