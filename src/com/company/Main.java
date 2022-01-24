641b963ba50a410d3482cbb48f393aa28754e6d6
amir <khamidullin.amir@gmail.com>
2022-01-24 23:34:45 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,20 +4,37 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-        Double x = scanner.nextDouble();
-        Double y = scanner.nextDouble();
+        String str = scanner.nextLine();
+        String[] strs = str.split(" ");
 
+        int[] x = new int[strs.length];
 
-        if ((x >= ((1- Math.sqrt(17)/2))) && (x <= ((1+ Math.sqrt(17)/2)))){
+        for (int i = 0; i < strs.length; i++){
 
-            if (y >= (x*x - 4) && y <= x){
+            x[i] = Integer.parseInt(strs[i]);
 
-                System.out.println("Yes!");
+        }
+
+        int counter = 0;
+        for(int i = 0; i < x.length; i++){
+
+            if(x[i] ==0){
+
+                counter = counter + 1;
+
+            }else{
+
+                System.out.print(x[i] + " ");
             }
 
         }
+        for(int i=0; i < counter; i++){
+
+            System.out.print("0 ");
+
+        }
+
 
     }
 }
